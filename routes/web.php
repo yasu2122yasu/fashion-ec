c399567a245001d2a6e1eb5e21c2dd441115d976
yasu2122yasu <ya2su1shi2@icloud.com>
2023-01-07 23:52:58 +0900

    55

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::controller(LineItemController::class)->group(function () {
 Route::controller(CartController::class)->group(function () {
     Route::name('cart.')->group(function () {
         Route::get('/cart', 'index')->name('index');
+        Route::get('/cart/chechkout', 'checkout')->name('checkout');
     });
 });
 
